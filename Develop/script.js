19f67c7c0023772d68bed8050f76abb33afb4fcb
Stephen Fike <sfike98@gmail.com>
2022-02-26 12:15:16 -0600

    added lower case randomization and arrays for pass values

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,50 @@
 // Assignment code here
- var generatePassword = function() {
-   
+var letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numers = [];
+var specialCharacters = [];
+var finalPassword = ""
+
+var getPasswordLength = function() {
+  var passLength = prompt("How long do you want the password");
+  return passLength;
  }
+
+ var lowerCase = function() {
+  var lowerCasePrompt = confirm("Do you want to include lower case characters?")
+  return lowerCasePrompt;
+ }
+
+var createRandomNumber = function() {
+  var randomNumber = Math.floor(Math.random() * letters.length);
+  return randomNumber;
+}
+
+var lowerCasePicker = function(num) {
+  var fractionalNum = num / 4;
+  while(fractionalNum > 0) {
+    finalPassword += letters[createRandomNumber()].toLowerCase();
+    fractionalNum--
+  }
+}
+
+function generatePassword() {
+  var passwordLength = getPasswordLength();
+  lowerCase();
+  lowerCasePicker(passwordLength);
+  lowerCasePicker(passwordLength);
+  lowerCasePicker(passwordLength);
+  lowerCasePicker(passwordLength);
+  lowerCasePicker(passwordLength);
+  checkFinalPassword(passwordLength);
+  console.log(finalPassword);
+ }
+
+var checkFinalPassword = function(num) {
+  if(finalPassword.length > num) {
+  var finalPasswordTrim = finalPassword.substring(0, parseInt(num));
+  finalPassword = finalPasswordTrim;
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
